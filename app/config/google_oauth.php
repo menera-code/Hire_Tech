b51fc373a73507f0c67a6c2a621bd976559f417f
Christian Mendoza <chimen0531@gmail.com>
2025-11-23 02:34:50 -0800

    First

--- app/config/google_oauth.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'client_id' => '255304170297-5fj6kbgmdgh7tpntjrahre9jocfntg0h.apps.googleusercontent.com',
-    'client_secret' => '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',
-    'redirect_uri' => 'http://localhost:3000/auth/google_callback'
-];
-?>
